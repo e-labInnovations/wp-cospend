1cb7af5a5aed29eb020f02e6f6f207806e648775
e-labinnovations <mohammedashad23@gmail.com>
2024-07-15 22:04:56 +0530

    db fix

--- docs/db.sql
+++ docs/db.sql
@@ -1,66 +1,76 @@
-CREATE TABLE wp_cospend_users (
-    id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE wp_cospend_members (
+    id BIGINT(20) UNSIGNED AUTO_INCREMENT,
     display_name VARCHAR(250) NOT NULL,
-    user_email VARCHAR(100) NOT NULL,
+    member_email VARCHAR(100),
     wp_user_id INT(20) UNSIGNED UNIQUE,
-    picture_id BIGINT(20) UNSIGNED
-);
+    picture_id BIGINT(20) UNSIGNED,
+    PRIMARY KEY id(id)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
 
 CREATE TABLE wp_cospend_groups (
-    id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT(20) UNSIGNED AUTO_INCREMENT,
     name VARCHAR(200) NOT NULL,
     created_at DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
     created_by INT(20) UNSIGNED NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    picture_id INT(20) UNSIGNED NOT NULL,
-    owner_id BIGINT(20) UNSIGNED NOT NULL
-);
+    description VARCHAR(255),
+    picture_id INT(20) UNSIGNED,
+    owner_id BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY id(id)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
 
 CREATE TABLE wp_cospend_group_members (
-    id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT(20) UNSIGNED AUTO_INCREMENT,
     group_id INT(20) UNSIGNED NOT NULL,
     user_id INT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY id(id),
     FOREIGN KEY (group_id) REFERENCES wp_cospend_groups(id),
-    FOREIGN KEY (user_id) REFERENCES wp_cospend_users(id)
-);
+    FOREIGN KEY (user_id) REFERENCES wp_cospend_members(id)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
 
 CREATE TABLE wp_cospend_expenses (
-    id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT(20) UNSIGNED AUTO_INCREMENT,
     group_id BIGINT(20) UNSIGNED NOT NULL,
     payer_id BIGINT(20) UNSIGNED NOT NULL,
     amount DECIMAL(10, 2) NOT NULL DEFAULT 0,
-    date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-    note LONGTEXT NOT NULL,
+    added_at DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+    note LONGTEXT,
     icon VARCHAR(200) NOT NULL,
     transaction_method_id BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY id(id),
     FOREIGN KEY (group_id) REFERENCES wp_cospend_groups(id),
-    FOREIGN KEY (payer_id) REFERENCES wp_cospend_users(id),
+    FOREIGN KEY (payer_id) REFERENCES wp_cospend_members(id),
     FOREIGN KEY (transaction_method_id) REFERENCES wp_cospend_transaction_methods(id)
-);
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
 
 CREATE TABLE wp_cospend_transactions (
-    id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT(20) UNSIGNED AUTO_INCREMENT,
     expense_id BIGINT(20) UNSIGNED NOT NULL,
-    from_user_id BIGINT(20) UNSIGNED NOT NULL,
-    to_user_id BIGINT(20) UNSIGNED NOT NULL,
+    from_member_id BIGINT(20) UNSIGNED NOT NULL,
+    to_member_id BIGINT(20) UNSIGNED NOT NULL,
     amount DECIMAL(10, 2) NOT NULL DEFAULT 0,
-    date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+    added_at DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+    transaction_method BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY id(id),
     FOREIGN KEY (expense_id) REFERENCES wp_cospend_expenses(id),
-    FOREIGN KEY (from_user_id) REFERENCES wp_cospend_users(id),
-    FOREIGN KEY (to_user_id) REFERENCES wp_cospend_users(id)
-);
+    FOREIGN KEY (from_member_id) REFERENCES wp_cospend_members(id),
+    FOREIGN KEY (to_member_id) REFERENCES wp_cospend_members(id)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
 
 CREATE TABLE wp_cospend_attachments (
-    id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT(20) UNSIGNED AUTO_INCREMENT,
     expense_id BIGINT(20) UNSIGNED NOT NULL,
     wp_media_id BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY id(id),
     FOREIGN KEY (expense_id) REFERENCES wp_cospend_expenses(id)
-);
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
 
 CREATE TABLE wp_cospend_transaction_methods (
-    id BIGINT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT(20) UNSIGNED AUTO_INCREMENT,
     name VARCHAR(200) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
     icon VARCHAR(100) NOT NULL,
-    picture_id INT(20) UNSIGNED
-);
+    picture_id INT(20) UNSIGNED,
+    group_id BIGINT(20) UNSIGNED NOT NULL,
+    PRIMARY KEY id(id),
+    FOREIGN KEY (group_id) REFERENCES wp_cospend_groups(id)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
--- includes/class-wp-cospend-db.php
+++ includes/class-wp-cospend-db.php
@@ -8,76 +8,83 @@ class WPCospend_DB {
     public static function create_tables() {
         global $wpdb;
         $charset_collate = $wpdb->get_charset_collate();
-        $table_users                = $wpdb->prefix . 'cospend_users';
-        $table_groups               = $wpdb->prefix . 'cospend_groups';
-        $table_group_members        = $wpdb->prefix . 'cospend_group_members';
-        $table_expenses             = $wpdb->prefix . 'cospend_expenses';
-        $table_transactions         = $wpdb->prefix . 'cospend_transactions';
-        $table_attachments          = $wpdb->prefix . 'cospend_attachments';
-        $table_transaction_methods  = $wpdb->prefix . 'cospend_transaction_methods';
+        $table_members       = $wpdb->prefix . 'cospend_members';
+        $table_groups        = $wpdb->prefix . 'cospend_groups';
+        $table_group_members = $wpdb->prefix . 'cospend_group_members';
+        $table_expenses      = $wpdb->prefix . 'cospend_expenses';
+        $table_transactions  = $wpdb->prefix . 'cospend_transactions';
+        $table_attachments   = $wpdb->prefix . 'cospend_attachments';
+        $table_payment_modes = $wpdb->prefix . 'cospend_payment_modes';
 
-
-        $tables = "
-            CREATE TABLE $table_users (
-                id BIGINT(20) UNSIGNED AUTO_INCREMENT,
+        $queries = "
+            CREATE TABLE $table_members (
+                id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
                 display_name VARCHAR(250) NOT NULL,
-                user_email VARCHAR(100) NOT NULL,
-                wp_user_id INT(20) UNSIGNED UNIQUE,
-                picture_id BIGINT(20) UNSIGNED
+                member_email VARCHAR(100),
+                wp_member_id INT(20) UNSIGNED UNIQUE,
+                picture_id BIGINT(20) UNSIGNED,
+                PRIMARY KEY (id)
             ) $charset_collate;
 
             CREATE TABLE $table_groups (
-                id BIGINT(20) UNSIGNED AUTO_INCREMENT,
+                id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
                 name VARCHAR(200) NOT NULL,
                 created_at DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
                 created_by INT(20) UNSIGNED NOT NULL,
-                description VARCHAR(255) NOT NULL,
-                picture_id INT(20) UNSIGNED NOT NULL,
-                owner_id BIGINT(20) UNSIGNED NOT NULL
+                description VARCHAR(255),
+                picture_id INT(20) UNSIGNED,
+                owner_id BIGINT(20) UNSIGNED NOT NULL,
+                PRIMARY KEY (id)
             ) $charset_collate;
 
             CREATE TABLE $table_group_members (
-                id INT(20) UNSIGNED AUTO_INCREMENT,
-                group_id INT(20) UNSIGNED NOT NULL,
-                user_id INT(20) UNSIGNED NOT NULL
+                id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+                group_id BIGINT(20) UNSIGNED NOT NULL,
+                member_id BIGINT(20) UNSIGNED NOT NULL,
+                PRIMARY KEY (id)
             ) $charset_collate;
 
             CREATE TABLE $table_expenses (
-                id BIGINT(20) UNSIGNED AUTO_INCREMENT,
+                id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
                 group_id BIGINT(20) UNSIGNED NOT NULL,
                 payer_id BIGINT(20) UNSIGNED NOT NULL,
                 amount DECIMAL(10, 2) NOT NULL DEFAULT 0,
                 date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
-                note LONGTEXT NOT NULL,
-                icon VARCHAR(200) NOT NULL,
-                transaction_method_id BIGINT(20) UNSIGNED NOT NULL
+                note LONGTEXT,
+                category VARCHAR(200) NOT NULL,
+                payment_mode_id BIGINT(20) UNSIGNED NOT NULL,
+                PRIMARY KEY (id)
             ) $charset_collate;
 
             CREATE TABLE $table_transactions (
-                id BIGINT(20) UNSIGNED AUTO_INCREMENT,
+                id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
                 expense_id BIGINT(20) UNSIGNED NOT NULL,
-                from_user_id BIGINT(20) UNSIGNED NOT NULL,
-                to_user_id BIGINT(20) UNSIGNED NOT NULL,
+                from_member_id BIGINT(20) UNSIGNED NOT NULL,
+                to_member_id BIGINT(20) UNSIGNED NOT NULL,
                 amount DECIMAL(10, 2) NOT NULL DEFAULT 0,
-                date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'
+                date DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
+                payment_mode_id BIGINT(20) UNSIGNED NOT NULL,
+                PRIMARY KEY (id)
             ) $charset_collate;
 
             CREATE TABLE $table_attachments (
-                id BIGINT(20) UNSIGNED AUTO_INCREMENT,
+                id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
                 expense_id BIGINT(20) UNSIGNED NOT NULL,
-                wp_media_id BIGINT(20) UNSIGNED NOT NULL
+                wp_media_id BIGINT(20) UNSIGNED NOT NULL,
+                PRIMARY KEY (id)
             ) $charset_collate;
 
-            CREATE TABLE $table_transaction_methods (
-                id BIGINT(20) UNSIGNED AUTO_INCREMENT,
+            CREATE TABLE $table_payment_modes (
+                id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
                 name VARCHAR(200) NOT NULL,
-                description VARCHAR(255) NOT NULL,
-                icon VARCHAR(100) NOT NULL,
-                picture_id INT(20) UNSIGNED
+                description VARCHAR(255),
+                picture_id INT(20) UNSIGNED,
+                group_id BIGINT(20) UNSIGNED NOT NULL,
+                PRIMARY KEY (id)
             ) $charset_collate;
         ";
 
         require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-        dbDelta($tables);
+        dbDelta($queries);
     }
 }
